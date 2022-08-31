e2bc3f0fc952d5c7d5ea30817d7c3a26e36f972b
Pallavi <pallavi.panchal2989@gmail.com>
2022-08-31 19:34:59 +0530

    Battery low

---
+++ src/beginner/bbasicprogrammingconcepts/BatteryLow.java
@@ -0,0 +1,18 @@
+package beginner.bbasicprogrammingconcepts;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class BatteryLow
+{
+    public static void main(String[] args) throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int tests = sc.nextInt();
+        while (tests-- > 0)
+        {
+            int level = sc.nextInt();
+            System.out.println(level > 15 ? "No" : "Yes");
+        }
+    }
+}
