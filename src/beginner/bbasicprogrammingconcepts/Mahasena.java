3e1040160aa1efb3fa51dcdb688eac66f9118f70
Pallavi <pallavi.panchal2989@gmail.com>
2022-09-01 09:35:46 +0530

    Mahasena

---
+++ src/beginner/bbasicprogrammingconcepts/Mahasena.java
@@ -0,0 +1,27 @@
+package beginner.bbasicprogrammingconcepts;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Mahasena
+{
+    public static void main(String[] args) throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int total = sc.nextInt();
+        int even = 0;
+        int odd = 0;
+
+        while (total > 0)
+        {
+            int weapons = sc.nextInt();
+            if(weapons % 2 == 0)
+                even++;
+            else
+                odd++;
+            total--;
+        }
+        System.out.println(even > odd ? "READY FOR BATTLE" : "NOT READY");
+    }
+
+}
