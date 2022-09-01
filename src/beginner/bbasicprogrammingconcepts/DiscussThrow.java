a83a4bee8b1b9197bb5ff2658ff0cdb64c95b95f
Pallavi <pallavi.panchal2989@gmail.com>
2022-09-01 17:35:21 +0530

    Discuss throw

---
+++ src/beginner/bbasicprogrammingconcepts/DiscussThrow.java
@@ -0,0 +1,20 @@
+package beginner.bbasicprogrammingconcepts;
+
+import java.util.Scanner;
+
+public class DiscussThrow {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tests = sc.nextInt();
+
+        while (tests > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            int max = Math.max(Math.max(a, b), c);
+            System.out.println(max);
+            tests--;
+        }
+    }
+}
