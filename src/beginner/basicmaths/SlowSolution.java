3e66f588119b690fb642f6711875bc6cfe9c4a8d
Pallavi <pallavi.panchal2989@gmail.com>
2022-12-11 11:54:26 +0530

    Slow Solution

---
+++ src/beginner/basicmaths/SlowSolution.java
@@ -0,0 +1,23 @@
+package beginner.basicmaths;
+
+import java.util.Scanner;
+
+public class SlowSolution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            int maxT = sc.nextInt();
+            int maxN = sc.nextInt();
+            int sumN = sc.nextInt();
+
+            int max = 0;
+            while (maxT > 0 && sumN > 0) {
+                max += (int) Math.pow(Math.min(maxN, sumN), 2);
+                sumN -= Math.min(maxN, sumN);
+                maxT--;
+            }
+            System.out.println(max);
+        }
+    }
+}
